40d8a184edeaada90d475ba078a18b960d7588db
Ryan Lau <rlau1185@jh-mba158-14.local>
2019-02-12 14:40:00 -0600

    added table view data source

--- hackwich:shoppingList/ViewController.swift
+++ hackwich:shoppingList/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var newItemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
-
+    @IBAction func newItemButtonPressed(_ sender: Any) {
+        
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
 
 }
 
